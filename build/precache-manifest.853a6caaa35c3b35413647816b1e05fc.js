b506280e23391f95a4868aff02e061f761c0911d
Syed Nazir <ksyednazir@gmail.com>
2019-05-01 08:41:16 +0400

    initial commit

---
+++ build/precache-manifest.853a6caaa35c3b35413647816b1e05fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d56708f665e763342790",
+    "url": "/static/js/main.44fe59b5.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "d56708f665e763342790",
+    "url": "/static/css/main.f7271ec3.chunk.css"
+  },
+  {
+    "revision": "2687adf96f707933c90a6760f1ce5754",
+    "url": "/index.html"
+  }
+];
